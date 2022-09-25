88edeb8ea89ee4b608efd5a09a3eab69140fb898
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2022-09-25 00:41:26 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# GLTF3dviewer
+version IV
+
+Once downloaded, do the following
+1)npm install
+2)npm run dev
+
+# Viewer.js
+On the gui.section, in case the user would like to see the Pointlight option, unlock it and save the changes.
---
+++ assets/environment/index.js
@@ -0,0 +1,13 @@
+export const environments = [
+  {
+    id: '',
+    name: 'None',
+    path: null,
+  },
+  {
+    id: 'office',
+    name: 'Office-Room',
+    path: 'assets/environment/equirectangular.hdr',
+    format: '.hdr'
+  }
+];
